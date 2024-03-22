979e3d1a846735a3e7b4f868fab6c76227349dfd
DuyChinh <doanduychinh2102@gmail.com>
2024-03-22 10:08:38 +0700

    exe2

--- readfile/app.py
+++ readfile/app.py
@@ -1,22 +1,102 @@
-try: 
-    f = open("chinh.txt", "r") 
-    # list_content = f.read()
-    list_content = f.readlines()
-    print(type(list_content))
-    i = 1
-    for content in list_content: 
-        print(str(i) + " : " + content)
-        i+=1
-    # print(content)
-    # --> read file
-    # f = open("chinh.txt", "a")
-    #--> write file(appending)
-    # f.write("Write file with Python 2!\n")
-    # f.write("Write file with Python 3!\n")
-    # f.write("Write file with Python 4!")
+import psycopg2
+from config import load_config
+from flask import Flask, request, render_template
+from flask_cors import CORS, cross_origin
+# from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__) #name contain current module
+
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+#--> Follow change
+
+#Connect PostgreSQL
+def connect(config):
+    """ Connect to the PostgreSQL database server """
+    try:
+        # connecting to the PostgreSQL server
+        with psycopg2.connect(**config) as conn:
+            print('Connected to the PostgreSQL server.')
+            return conn
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
     
-    # print(f.read())
-except Exception as e:
-    print(e)
-finally:
-    f.close()
+# có các column là các cột trong file csv này
+# sau đó viết code python load dữ liệu vào postgres nhé
+# rồi sử dụng project flask hôm trước viết thêm 1 route orders, để query orders trong file này ra, có hỗ trợ filter theo các params
+
+@app.route("/") 
+@cross_origin(origin='*')
+
+def home():
+    return "API LOGIN"
+
+def printData(list_content): 
+    print(list_content)
+
+def create_table():
+    print("creating")
+    cur = conn.cursor()
+    create_table_query = '''CREATE TABLE orders (
+        id INTEGER PRIMARY KEY,
+        order_priority CHARACTER VARYING,
+        discount NUMERIC,
+        unit_price NUMERIC, 
+        shipping_cost NUMERIC,
+        customer_id INTEGER,
+        customer_name CHARACTER VARYING,
+        ship_mode CHARACTER VARYING, 
+        customer_segment CHARACTER VARYING,
+        product_category CHARACTER VARYING,
+        product_subCategory CHARACTER VARYING,
+        product_container CHARACTER VARYING,
+        product_name CHARACTER VARYING,
+        product_base_margin NUMERIC,
+        region CHARACTER VARYING,
+        state_or_province CHARACTER VARYING,
+        city CHARACTER VARYING,
+        postal_code INTEGER,
+        order_date DATE,
+        ship_date DATE,
+        profit NUMERIC,
+        quantity_ordered_new INTEGER,
+        sales NUMERIC,
+        order_id INTEGER
+    )'''
+    try: 
+        cur.execute(create_table_query)
+        print("Create table success")
+    except:
+        print("Fail")
+    
+
+def table_exists(table_name, cursor):
+    cursor.execute("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = %s)", (table_name,))
+    return cursor.fetchone()[0]
+
+if __name__ == '__main__':
+    config = load_config()
+    conn = connect(config)
+    cur = conn.cursor()
+    if not table_exists('orders', cur):
+        create_table()
+
+    cur.execute('SELECT * FROM orders')
+    orders = cur.fetchall()
+    print(orders)
+    # app.run(host='0.0.0.0', port='6868') 
+    
+
+
+
+
+
+# try:
+#     f = open("Orders.csv", "r") 
+#     list_content = f.readline().strip().split(',')
+#     # printData(list_content)
+#     create_table()
+# except Exception as e:
+#         print(e)
+# finally:
+#         f.close()
+#         conn.close()
